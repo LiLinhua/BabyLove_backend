67f1202218e85d2a39de409ccc996cf6db6345bb
LiLinhua <1009346921@qq.com>
2023-10-07 21:41:15 +0800

    feat: 更新前端静态资源

--- app/public/babylove/p__index.5ac68233.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[866,548],{12997:function(t,u,s){s.r(u);var l=s(28477);u.default=l.default}}]);
---
+++ app/public/babylove/p__index.dee71e33.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[866,548],{12997:function(t,u,s){s.r(u);var l=s(93550);u.default=l.default}}]);
